3da50f9bd141c27fa137b904d6effcb1c903a0f6
ryckabass0827 <laiskasrichardui@gmail.com>
2023-01-04 13:57:30 +0200

    added alt

---
+++ youtube uzduotis/script/main.js
@@ -0,0 +1,17 @@
+function displayCard(cardData) {
+    const card = document.querySelector('.card');
+
+    card.querySelector('img').src = cardData.imageUrl;
+    card.querySelector('.name').textContent = cardData.name;
+    card.querySelector('.time').textContent = cardData.time;
+    card.querySelector('.creator-icon').src = cardData.creatorIcon;
+    card.querySelector('.views').textContent = cardData.views;
+}
+
+fetch('./data/data.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        displayCard(data.card[0]);
+    });
+
